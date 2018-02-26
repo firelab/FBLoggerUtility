1eba06cbc6cd707ab09df0a643ef1bc7b323471e
jforthofer <jforthofer@gmail.com>
2018-02-26 15:12:00 -0700

    Fixed HFT calculation, which involved changing the negative value output by the logger to a positive value.  The actual number is correct, just not the sign.

--- FBLoggerUtility/FBLoggerDataReader.cpp
+++ FBLoggerUtility/FBLoggerDataReader.cpp
@@ -375,7 +375,9 @@ double FBLoggerDataReader::CalculateHeatFlux(double rawVoltage)
 
 double FBLoggerDataReader::CalculateHeatFluxTemperature(double rawVoltage)
 {
-    double thermalResistance = 10000.0 / ((1.25 / rawVoltage) - 1.0);
+	if (rawVoltage < 0.0)	//for some reason logger is outputting negative voltage, make it positive
+		rawVoltage = -rawVoltage;
+	double thermalResistance = 10000.0 / ((1.25 / rawVoltage) - 1.0);
     double heatFluxTemperature = (1.0 / (0.003356 - (log(30000.0 / thermalResistance) / 3962.0))) - 273.15;
     return heatFluxTemperature;
 }
