e2ce93a846f3c3809dca08d755c899e5b241d37f
Jason Forthofer <jforthofer@gmail.com>
2024-12-31 16:23:21 -0700

    Just testing a git access issue, not code changes.

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ int main() {
     std::cout << "Beginning conversion..." << std::endl;
 
 
+
     std::vector<double> angles_, ReynoldsNumbers_;
     std::vector<std::vector<double>> pressureCoefficients_;
 
@@ -33,14 +34,14 @@ int main() {
             double angle, ReynoldsNumber, pressureCoefficient;
             string line;
 
-            std::ifstream windTunnelDataFile("/home/jforthofer/src/FBLoggerUtility/wind_tunnel_data/TG201811071345.txt", std::ios::in);
-            const int NUM_ANGLES = 100;
-            const int NUM_REYNOLDS_NUMBERS = 11;
-            const int NUM_DATA_LINES = NUM_ANGLES * NUM_REYNOLDS_NUMBERS;
-//            std::ifstream windTunnelDataFile("/home/jforthofer/src/FBLoggerUtility/wind_tunnel_data/TG202411061143.txt", std::ios::in);
-//            const int NUM_ANGLES = 10;
-//            const int NUM_REYNOLDS_NUMBERS = 5;
+//            std::ifstream windTunnelDataFile("/home/jforthofer/src/FBLoggerUtility/wind_tunnel_data/TG201811071345.txt", std::ios::in);
+//            const int NUM_ANGLES = 100;
+//            const int NUM_REYNOLDS_NUMBERS = 11;
 //            const int NUM_DATA_LINES = NUM_ANGLES * NUM_REYNOLDS_NUMBERS;
+            std::ifstream windTunnelDataFile("/home/jforthofer/src/FBLoggerUtility/wind_tunnel_data/TG202411061143.txt", std::ios::in);
+            const int NUM_ANGLES = 10;
+            const int NUM_REYNOLDS_NUMBERS = 5;
+            const int NUM_DATA_LINES = NUM_ANGLES * NUM_REYNOLDS_NUMBERS;
 
             bool isSuccessful = false;
             angles_.clear();
